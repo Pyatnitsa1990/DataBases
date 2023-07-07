8ba578b3c7e38a75631e391f328625c2d9c4f19d
Piatnytsia Oleksii <pyatnitsa90@gmail.com>
2023-07-08 00:09:02 +0300

    change file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.homework17.model.Topic;
+import org.example.homework17.repository.TopicRepositoryImpl;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world");
     }
 }
